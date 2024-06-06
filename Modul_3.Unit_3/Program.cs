6c456b81c0c7cb37418d11a2c6b38320e6b40dc0
DeniValuev <valuev.denis91@mail.ru>
2024-06-06 22:15:26 +0300

    Добавьте файлы проекта.

---
+++ Modul_3.Unit_3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Modul_3.Unit_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Denis";
+            Console.WriteLine(MyName);
+        }
+    }
+}
