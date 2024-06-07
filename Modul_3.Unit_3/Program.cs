c62959f1a43b155158bcbafb9919fab955541eca
DeniValuev <valuev.denis91@mail.ru>
2024-06-07 09:40:52 +0300

    Module_3-master: Add project Unit_3 and Unit_4

--- Modul_3.Unit_3/Program.cs
+++ Modul_3.Unit_3/Program.cs
@@ -1,11 +1,10 @@
-﻿namespace Modul_3.Unit_3
+﻿namespace Module_3.Unit_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Denis";
+            const string MyName = "Denis";
             Console.WriteLine(MyName);
         }
     }
---
+++ Module_3.Unit_4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Module_3.Unit_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIAPet = true;
+            double MyShoeSize = 37.5;
+            Console.WriteLine("My name is {0}", MyName);
+            Console.WriteLine("My age is {0}", MyAge);
+            Console.WriteLine("Do i have a pet? {0}", HaveIAPet);
+            Console.WriteLine("My shoe size is {0}", MyShoeSize);
+        }
+    }
+}
